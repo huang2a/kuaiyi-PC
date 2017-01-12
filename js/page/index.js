dffa7ad0a8ad6d363c32174d2afbb5dafc32af1e
huang2a <huang2a>
2017-01-12 17:58:17 +0800

    update  invest fill index page

---
+++ js/page/index.js
@@ -0,0 +1,32 @@
+
+var swiper = new Swiper('.banner-roll', {
+    pagination: '.banner-dot',
+    autoplay : 5000,
+    paginationClickable: true
+});
+
+var swiper = new Swiper('.items-roll1', {
+    slidesPerView: 2,
+    nextButton: '.items-roll1-next',
+    prevButton: '.items-roll1-prev',
+});
+
+var swiper = new Swiper('.volume-roll', {
+    direction: 'vertical',
+    slidesPerView : 6,
+    autoplay : 1500,
+    paginationClickable : true,
+    spaceBetween: 6,
+});
+
+
+$(".coupon .close").click(function(){
+  $(".coupon .img").hide();
+  $(this).hide();
+});
+
+$(".gotop a").click(function () {
+        var speed=200;//滑动的速度
+        $('body,html').animate({ scrollTop: 0 }, speed);
+        return false;
+ });
