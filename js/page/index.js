ec529dfe0589ec4e312bdba4dee6327a48dbf826
Cherryzrc <Cherryzrc@163.com>
2017-01-13 12:03:17 +0800

    setarror

--- js/page/index.js
+++ js/page/index.js
@@ -1,23 +1,5 @@
 
-var swiper = new Swiper('.banner-roll', {
-    pagination: '.banner-dot',
-    autoplay : 5000,
-    paginationClickable: true
-});
 
-var swiper = new Swiper('.items-roll1', {
-    slidesPerView: 2,
-    nextButton: '.items-roll1-next',
-    prevButton: '.items-roll1-prev',
-});
-
-var swiper = new Swiper('.volume-roll', {
-    direction: 'vertical',
-    slidesPerView : 6,
-    autoplay : 1500,
-    paginationClickable : true,
-    spaceBetween: 6,
-});
 
 
 $(".coupon .close").click(function(){
